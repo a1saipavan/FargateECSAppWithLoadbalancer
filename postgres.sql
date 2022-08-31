35c92f6e54492cf8cfb3afc23b3bf9fa88c544fc
a1saipavan <76589153+a1saipavan@users.noreply.github.com>
2022-08-31 14:55:16 +0530

    ssbs

--- postgres.sql
+++ postgres.sql
@@ -1,1 +1,8 @@
-createdb mysql;
+CREATE TABLE accounts (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP 
+);
