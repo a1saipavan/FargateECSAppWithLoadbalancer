8121a2165a504ea608d418d7f9128463912cd4fe
a1saipavan <76589153+a1saipavan@users.noreply.github.com>
2022-09-04 21:27:23 +0530

    sdbsbsh

--- postgres.sql
+++ postgres.sql
@@ -1,1 +1,3 @@
-psql -c "CREATE TABLE test1 (device SERIAL)"
+CREATE TABLE clients (id SERIAL PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, role VARCHAR);
+
+INSERT INTO clients (first_name, last_name, role) VALUES ('John', 'Smith', 'CEO');
